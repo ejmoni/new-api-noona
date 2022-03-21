a6b3fbf9534cc10ab9fa05e48348b011609e6a89
Eunjoo Lee <isej0916@gmail.com>
2022-03-21 16:39:28 +0900

    api arror control

--- main.js
+++ main.js
@@ -1,34 +1,57 @@
 let news = [];
+let page = 1;
+let total_pages =0;
 let menus = document.querySelectorAll(".menus button")
 menus.forEach(menu=> menu.addEventListener("click", (event)=>getNewsByTopic(event) ))
 
 let searchButton = document.getElementById("search-button");
+let url;
+
+const getNews =async ()=>{
+    try{
+        let header = new Headers({
+            'x-api-key':'gKtRfPkzF2YewXzsSMa3d0Fjgs6zmkkvJAoY6pVB1m4'
+        });
+        url.searchParams.set('page', page);
+        console.log("url은?",url)
+        let response =  await fetch(url,{headers:header});
+        let data = await response.json();
+        if(response.status == 200){
+            if(data.total_hits == 0 ){
+                throw new Error("검색된 결과값이 없습니다.");
+            }
+            console.log("받는데이터가 뭐지?", data);
+            news = data.articles;
+            total_pages = data.total_pages;
+            page = data.page;
+            console.log(news);
+            render();
+            pagenation();
+        }else {
+            throw new Error(data.massage);
+        }
+      
+    }catch(error){
+        console.log("잡힌 에러는",error.message);
+        errorRender(error.message);
+    }
+
+};
 
 
 const getLatestNews = async()=>{
-    let url =  new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10'
+     url =  new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10'
     );
-    let header = new Headers({
-        'x-api-key':'gKtRfPkzF2YewXzsSMa3d0Fjgs6zmkkvJAoY6pVB1m4'
-    });
-    let response =  await fetch(url,{headers:header});
-    let data = await response.json();
-    news = data.articles
-    console.log(news);
-
-   render();
+    getNews();
 };
 
 const getNewsByTopic = async(event) =>{
     console.log("클림됨", event.target.textContent);
     let topic = event.target.textContent.toLowerCase()
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`);
-    
-    let header = new Headers({'x-api-key':'gKtRfPkzF2YewXzsSMa3d0Fjgs6zmkkvJAoY6pVB1m4'});
-    let response =  await fetch(url,{headers:header});
-    let data = await response.json();
-    news=data.articles
-    render()
+     url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
+    );
+    getNews();
+   
 };
 
 const getNewsByKeyword =async()=>{
@@ -39,17 +62,10 @@ const getNewsByKeyword =async()=>{
     //데이터가져오기
     //데이터보여주기
     let keyword = document.getElementById("search-input").value;
-    let url = new URL(
+     url = new URL(
         `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10
         `);
-    let header = new Headers({
-     'x-api-key':'gKtRfPkzF2YewXzsSMa3d0Fjgs6zmkkvJAoY6pVB1m4'
-    });
-    let response =  await fetch(url,{headers:header});
-    let data = await response.json();
-    news = data.articles;
-    
-    render();
+    getNews();
 };
 
 
@@ -87,6 +103,55 @@ const render = () =>{
 
     document.getElementById("news-board").innerHTML=newsHTML;
 };
+
+const errorRender = (message) =>{
+    let errorHTML = `<div class="alert alert-danger text-center" role="alert">
+    ${message}
+  </div>`;
+    document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const pagenation = () =>{
+    let pagenationHTML =``;
+    //total
+    //page
+    //page group
+    let pageGroup = Math.ceil(page/5);
+    //last
+    let last = pageGroup*5
+    //first
+    let first = last-4
+    //first~last print
+    
+    pagenationHTML = `<ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${page-1})">
+        <span aria-hidden="true">&lt;</span>
+      </a>
+    </li>`;
+
+
+    for(let i = first;i<=last;i++){
+        pagenationHTML+=`
+        <li class="page-item ${page-1}
+            page==i?"active":""
+        }"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`;
+    }
+    
+    pagenationHTML += `<li class="page-item">
+    <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${page+1})">
+      <span aria-hidden="true">&gt;</span>
+    </a>
+  </li>`;
+    
+    document.querySelector(".pagination").innerHTML=pagenationHTML;
+};
+
+const moveToPage =(pageNum) =>{
+    page = pageNum;
+
+    getNews();
+}
 
 searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
